7254a0c4d1ac23a577ae67b52a9331e6d4fae894
Cristian David Parra Zuluaga <cristian.parra.contacto@gmail.com>
2020-12-21 16:18:41 -0500

    Adding more versioning strategy: Request Param, Media-type (Content Negotiation and Accept Header) and Header strategy.

--- Spring_boot_Restful_API/restful-api/src/main/java/com/crisdavidpz/restfulapi/versioning/PersonVersioningController.java
+++ Spring_boot_Restful_API/restful-api/src/main/java/com/crisdavidpz/restfulapi/versioning/PersonVersioningController.java
@@ -15,4 +15,34 @@ public class PersonVersioningController {
 	public PersonV2 retrievePersonV2() {
 		return new PersonV2(new Name("Solecito", "Ramírez"));
 	}
+	
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 retrievePersonV1withParam() {
+		return new PersonV1("David el Gnomo");
+	}
+	
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 retrievePersonV2withParam() {
+		return new PersonV2(new Name("Solecito", "Ramírez"));
+	}
+	
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 retrievePersonV1withHeader() {
+		return new PersonV1("David el Gnomo");
+	}
+	
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 retrievePersonV2withHeader() {
+		return new PersonV2(new Name("Solecito", "Ramírez"));
+	}
+	
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 retrievePersonV1withProduces() {
+		return new PersonV1("David el Gnomo");
+	}
+	
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 retrievePersonV2withProduces() {
+		return new PersonV2(new Name("Solecito", "Ramírez"));
+	}
 }
