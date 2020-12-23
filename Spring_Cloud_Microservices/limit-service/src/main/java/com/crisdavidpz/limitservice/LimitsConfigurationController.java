7b3555fc6b93da8a2b46e3b9fcb8e0027ce86c56
Cristian David Parra Zuluaga <cristian.parra.contacto@gmail.com>
2020-12-23 14:36:29 -0500

    Enhancing limit service getting config from application.properties.

--- Spring_Cloud_Microservices/limit-service/src/main/java/com/crisdavidpz/limitservice/LimitsConfigurationController.java
+++ Spring_Cloud_Microservices/limit-service/src/main/java/com/crisdavidpz/limitservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.crisdavidpz.limitservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,13 @@ import com.crisdavidpz.limitservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 
 }
